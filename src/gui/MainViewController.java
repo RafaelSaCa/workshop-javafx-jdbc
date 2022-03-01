5cbb4666708f83c298f416d7b6e4f0f6ff5a85bd
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-01 16:43:40 -0300

    Initializing Action as Parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -34,13 +35,17 @@ public class MainViewController implements Initializable {
 	}
 
 	@FXML
-	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");;
+	public void onMenuItemDepartmentAction() {//expressao lambda como parametro / funcao para nao criar varia telas loadView 
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+				controller.setDepartmentService(new DepartmentService());
+				controller.updateTableView();
+		});
+		
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -48,30 +53,7 @@ public class MainViewController implements Initializable {
 
 	}
 	/* abrir outra tela*/
-	private synchronized void loadView(String absoluteName) {// passa o caminho das janelas
-		try {
-
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);	
-		}
-	}
-	
-	
-	
-	
-	/* mostra a lista de DEPARTAMENTO na tela */
-	private synchronized void loadView2 (String absoluteName) {// passa o caminho das janelas
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {// passa o caminho das janelas
 		try {
 
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -85,14 +67,17 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			//executa a funcao da expressao lambda//
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);	
 		}
 	}
+	
+	
+	
 	
 	
 }
