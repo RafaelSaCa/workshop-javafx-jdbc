a97702f5191ef8154cb43ae3504624b7a7913ffd
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-01 10:48:40 -0300

    Department Service

---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,22 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+
+	public List<Department> findAll(){
+		/*dados MOCKADOS nao acessados direto do banco somente teste*/
+		List<Department> list = new ArrayList<Department>();
+		list.add(new Department(1,"Livros"));
+		list.add(new Department(2,"Computadores"));
+		list.add(new Department(3,"Eletronicos"));
+		return list;
+		
+		
+	}
+	
+	
+}
